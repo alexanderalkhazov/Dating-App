647b82ce4b1c2ba25259790f87bd884d8435893b
Your Name <you@example.com>
2023-06-06 14:34:33 +0300

    Adding Angular And Setting Things Up

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -14,7 +14,11 @@ internal class Program
       opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
 
+    builder.Services.AddCors();
+
     var app = builder.Build();
+
+    app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
     app.MapControllers();
 
