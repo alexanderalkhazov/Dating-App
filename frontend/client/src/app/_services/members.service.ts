823f3ab56ba2055707280b6cd33f34d4dd5858cf
Your Name <you@example.com>
2023-06-14 13:09:14 +0300

    like feature added

--- frontend/client/src/app/_services/members.service.ts
+++ frontend/client/src/app/_services/members.service.ts
@@ -16,13 +16,13 @@ export class MembersService {
   baseUrl: string = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
-  userParams : UserParams | undefined;
+  userParams: UserParams | undefined;
   user: User | undefined;
-  
-  constructor(private httpClient: HttpClient ,private accountService: AccountService) { 
+
+  constructor(private httpClient: HttpClient, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => {
-        if (user){
+        if (user) {
           this.userParams = new UserParams(user);
           this.user = user;
         }
@@ -53,26 +53,26 @@ export class MembersService {
 
     if (response) return of(response);
 
-    let params = this.GetPaginationHeaders(userParams.pageNumber,userParams.pageSize);
+    let params = this.GetPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
-    params = params.append('minAge',userParams.minAge);
-    params = params.append('maxAge',userParams.maxAge);
-    params = params.append('gender',userParams.gender);
-    params = params.append('orderBy',userParams.orderBy);
+    params = params.append('minAge', userParams.minAge);
+    params = params.append('maxAge', userParams.maxAge);
+    params = params.append('gender', userParams.gender);
+    params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users',params).pipe(
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
       map(res => {
-        this.memberCache.set(Object.values(userParams).join('-'),res);
+        this.memberCache.set(Object.values(userParams).join('-'), res);
         return res;
       })
     );
   }
 
-  
+
   getMember(username: string) {
     const member = [...this.memberCache.values()]
-    .reduce((arr,elem) => arr.concat(elem.result),[])
-    .find((member: Member) => member.userName === username)
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username)
     if (member) return of(member);
     return this.httpClient.get<Member>(this.baseUrl + 'users/' + username);
   }
@@ -82,19 +82,30 @@ export class MembersService {
     return this.httpClient.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
-        this.members[index] = {...this.members[index] , ...member}
+        this.members[index] = { ...this.members[index], ...member }
       })
     );
   }
-  
-  setMainPhoto(photoId: number){
+
+  setMainPhoto(photoId: number) {
     return this.httpClient.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
   }
-  
-  deletePhoto(photoId: number){
+
+  deletePhoto(photoId: number) {
     return this.httpClient.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
-  private getPaginatedResult<T>(url:string , params: HttpParams) {
+
+  addLike(username: string) {
+    return this.httpClient.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string,pageNumber: number , pageSize: number) {
+    let params = this.GetPaginationHeaders(pageNumber,pageSize);
+    params = params.append('predicate',predicate);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes' , params);
+  }
+
+  private getPaginatedResult<T>(url: string, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.httpClient.get<T>(url, { observe: 'response', params }).pipe(
       map(response => {
@@ -110,10 +121,10 @@ export class MembersService {
     );
   }
 
-  private GetPaginationHeaders(pageNumber:  number, pageSize: number) {
-      let params = new HttpParams();
-      params = params.append('pageNumber', pageNumber);
-      params = params.append('pageSize', pageSize);
-      return params;
+  private GetPaginationHeaders(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+    return params;
   }
 }
